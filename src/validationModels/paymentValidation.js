cf99b8debc4a2a31fa99ffff3277bc1a40e90a7e
Ross Faulds <ross.faulds@bjss.com>
2018-03-13 12:48:10 +0000

    Fix bugs in create and update

--- src/validationModels/paymentValidation.js
+++ src/validationModels/paymentValidation.js
@@ -3,14 +3,14 @@ import Joi from 'joi';
 const paymentDetailsSchema = {
 	PaymentRef: Joi.string(),
 	AuthCode: Joi.string(),
-	PaymentAmount: Joi.number().integer(),
+	PaymentAmount: Joi.number().integer().min(10).max(9999),
 	PaymentDate: Joi.number().integer(),
 };
 
 
 export default {
 	request: Joi.object().keys({
-		PenaltyReference: Joi.string().required(),
+		PenaltyReference: Joi.string().required().regex(/^[a-zA-Z0-9-]{5,15}$/),
 		PenaltyType: Joi.string().regex(/^(IM|CDN|FPN)$/).required(),
 		PenaltyStatus: Joi.string().regex(/^(PAID|UNPAID)$/).required(),
 		PaymentDetail: Joi.object(paymentDetailsSchema).required(),
